68e1560adcf488ed0a0078977f04c191382d4ca4
bamdad1381 <bamdadnikkhah@gmail.com>
2016-06-03 01:09:45 +0430

    Update and rename version.lua to Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,41 @@
+do
+
+function run(msg, matches)
+  return [[➖➖➖☇Turbo☇➖➖➖
+Best Anti Spam & Funny Bot Based On TeleSeed & TG-Cli
+
+☇Sudo☇
+@MrJacki (Manager,Developer & Founder)
+🌟Admin🌟
+1_@Alfroshotak (Manager,Founder ...)
+2_@MrBlackLife (Developer,Supporter ...)
+3_@Xx_Nova_xX  (Founder,Supporter ...)
+and ...
+
+Special Tnx To:
+SEEDTeam
+Fucker Team
+BlackLife
+
+☇Bot Username:
+@TurboTG
+📡Channel Username:
+@Turbo_Team
+💡Support Bot:
+@Turbo_Support_Bot
+]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^[!/]version$",
+    "^[!/]ver$",
+    "^[Vv]ersion$",
+    "^[Vv]er$"
+  }, 
+  run = run 
+}
+
+end
