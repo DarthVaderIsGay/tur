c125bd85f138672364f6b761a979472625e4e34e
bamdad1381 <bamdadnikkhah@gmail.com>
2016-06-02 18:58:46 +0430

    Update and rename addsudoes.lua to AddSudo.lua

--- plugins/AddSudo.lua
+++ plugins/AddSudo.lua
@@ -4,7 +4,7 @@ vardump(success)
 vardump(result)
 end
 local function run(msg, matches)
-local sudo = 85831686 --Put you id Here !
+local sudo = 190142886 --Put you id Here !
 local addsudo = 'user#id'..sudo
 local chat = get_receiver(msg)
 if is_momod(msg) then -- you can set it to is_owner(msg)
@@ -18,9 +18,9 @@ moderator = {
 "!addsudo : invite Sudo to your group" },
 },
 patterns = {
-"^[~!/][Aa][Dd][Dd][Ss][uU][Dd][oO]$",
+"^[#!/][Aa][Dd][Dd][Ss][uU][Dd][oO]$",
 "^[Aa][Dd][Dd][Ss][uU][Dd][oO]$",
-"^[~!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+"^[#!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
 "^[Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
 },
 run = run
